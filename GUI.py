bf0284b46d38c5cb71c2f8a0dee8a8b5724dadee
Boh Shin Yeo <e0968907@u.nus.edu>
2024-12-02 16:09:50 +0800

    Added GUI

---
+++ GUI.py
@@ -0,0 +1,291 @@
+import tkinter as tk
+import os
+from Matcher import Matcher
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+
+file_name_data = []
+exchanger_data = []
+exchanger_preference = []
+buddy_data = []
+buddy_preference = []
+
+window = tk.Tk()
+window.title("Buddy Matching")
+window.geometry("600x600")
+window.resizable(True, True)
+
+# Create a scrollable frame within the window without a scollbar
+canvas = tk.Canvas(window, highlightthickness=0)
+scrollable_frame = tk.Frame(canvas, highlightthickness=0)
+scrollable_frame.bind("<Configure>", lambda e: canvas.configure(
+    scrollregion=canvas.bbox("all")))
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas.pack(side="left", fill="both", expand=True)
+
+# Allow the user to scroll with the mouse wheel
+
+
+def _on_mousewheel(event):
+    if event.delta > 0:
+        canvas.yview_scroll(-1, "units")
+    else:
+        canvas.yview_scroll(1, "units")
+
+
+canvas.bind_all("<MouseWheel>", _on_mousewheel)
+
+
+# ALlow the user to scroll with the arrow keys
+
+
+def _on_arrow_key(event):
+    if event.keysym == "Up":
+        canvas.yview_scroll(-1, "units")
+    elif event.keysym == "Down":
+        canvas.yview_scroll(1, "units")
+    elif event.keysym == "Page_Up":
+        canvas.yview_scroll(-3, "pages")
+    elif event.keysym == "Page_Down":
+        canvas.yview_scroll(3, "pages")
+
+
+canvas.bind_all("<Up>", _on_arrow_key)
+canvas.bind_all("<Down>", _on_arrow_key)
+canvas.bind_all("<Prior>", _on_arrow_key)
+canvas.bind_all("<Next>", _on_arrow_key)
+
+
+def add_column(frame):
+    row = frame.grid_size()[1]
+    temp_entry = tk.Entry(frame, width=60)
+    temp_entry.insert(0, "Column Name")
+    temp_entry.grid(row=row, column=0, sticky=tk.W)
+
+
+def delete_column(frame, column_name, delete_entry):
+    for entry in frame.winfo_children():
+        if isinstance(entry, tk.Entry) and entry.get() == column_name:
+            entry.destroy()
+            break
+    delete_entry.delete(0, tk.END)
+    delete_entry.insert(0, "Name of Column to Delete")
+
+
+def add_labels(data, frame, row=1):
+    for entry in data:
+        temp_entry = tk.Entry(frame, width=60)
+        temp_entry.insert(0, entry)
+        temp_entry.grid(row=row, column=0, sticky=tk.W)
+        row += 1
+
+
+def add_preference_label(data, frame):
+    faculty = tk.Label(frame, text="Faculty: ")
+    faculty.grid(row=1, column=0, sticky=tk.W)
+    faculty_entry = tk.Entry(frame, width=40)
+    faculty_entry.insert(0, data[0])
+    faculty_entry.grid(row=1, column=0, sticky=tk.W, padx=180)
+    match_faculty = tk.Label(frame, text="Match Faculty Preference: ")
+    match_faculty.grid(row=2, column=0, sticky=tk.W)
+    match_faculty_entry = tk.Entry(frame, width=40)
+    match_faculty_entry.insert(0, data[1])
+    match_faculty_entry.grid(row=2, column=0, sticky=tk.W, padx=180)
+    gender = tk.Label(frame, text="Gender: ")
+    gender.grid(row=3, column=0, sticky=tk.W)
+    gender_entry = tk.Entry(frame, width=40)
+    gender_entry.insert(0, data[2])
+    gender_entry.grid(row=3, column=0, sticky=tk.W, padx=180)
+    match_gender = tk.Label(frame, text="Match Gender Preference: ")
+    match_gender.grid(row=4, column=0, sticky=tk.W)
+    match_gender_entry = tk.Entry(frame, width=40)
+    match_gender_entry.insert(0, data[3])
+    match_gender_entry.grid(row=4, column=0, sticky=tk.W, padx=180)
+    interests = tk.Label(frame, text="Interests: ")
+    interests.grid(row=5, column=0, sticky=tk.W)
+    interests_entry = tk.Entry(frame, width=40)
+    interests_entry.insert(0, data[4])
+    interests_entry.grid(row=5, column=0, sticky=tk.W, padx=180)
+
+
+def fill_file_name_data(data, frame):
+    file_name_title = tk.Label(
+        frame, text="Input File Names Below: ")
+    file_name_title.grid(row=0, column=0, sticky=tk.W)
+    exchanger_file_name = tk.Label(
+        frame, text="Exchanger Excel File Name: ")
+    exchanger_file_name.grid(row=1, column=0, sticky=tk.W)
+    exchanger_entry = tk.Entry(frame, width=30)
+    if len(data) > 0:
+        exchanger_entry.insert(0, data[0])
+    exchanger_entry.grid(row=1, column=2)
+    buddy_file_name = tk.Label(frame, text="Buddy Excel File Name: ")
+    buddy_file_name.grid(row=2, column=0, sticky=tk.W)
+    buddy_entry = tk.Entry(frame, width=30)
+    if len(data) > 1:
+        buddy_entry.insert(0, data[1])
+    buddy_entry.grid(row=2, column=2)
+    output_file_name = tk.Label(
+        frame, text="Matching Output Excel File Name: ")
+    output_file_name.grid(row=3, column=0, sticky=tk.W)
+    output_entry = tk.Entry(frame, width=30)
+    if len(data) > 2:
+        output_entry.insert(0, data[2])
+    output_entry.grid(row=3, column=2)
+
+
+def save():
+    file_name_data.clear()
+    exchanger_data.clear()
+    exchanger_preference.clear()
+    buddy_data.clear()
+    buddy_preference.clear()
+
+    for widget in scrollable_frame.winfo_children():
+        if isinstance(widget, tk.Frame):
+            for entry in widget.winfo_children():
+                if isinstance(entry, tk.Entry):
+                    if widget.winfo_children()[0].cget("text") == "Input File Names Below: ":
+                        file_name_data.append(entry.get())
+                    elif widget.winfo_children()[0].cget("text") == "Input the Name of Exchanger Excel Columns Below: ":
+                        exchanger_data.append(entry.get())
+                    elif widget.winfo_children()[0].cget("text") == "Input the Name of Compulsory Columns for Exchanger Below: ":
+                        exchanger_preference.append(entry.get())
+                    elif widget.winfo_children()[0].cget("text") == "Input the Name of Buddy Excel Columns Below: ":
+                        buddy_data.append(entry.get())
+                    elif widget.winfo_children()[0].cget("text") == "Input the Name of Compulsory Columns for Buddy Below: ":
+                        buddy_preference.append(entry.get())
+
+    with open(os.path.join(file_path, "data.txt"), "w") as data_file:
+        for entry in file_name_data:
+            data_file.write(entry + "\n")
+        data_file.write("end!@#$%^&*()\n")
+        for entry in exchanger_data:
+            data_file.write(entry + "\n")
+        data_file.write("end!@#$%^&*()\n")
+        for entry in exchanger_preference:
+            data_file.write(entry + "\n")
+        data_file.write("end!@#$%^&*()\n")
+        for entry in buddy_data:
+            data_file.write(entry + "\n")
+        data_file.write("end!@#$%^&*()\n")
+        for entry in buddy_preference:
+            data_file.write(entry + "\n")
+        data_file.write("end!@#$%^&*()\n")
+
+    temp_label = tk.Label(scrollable_frame, text="Data saved successfully!")
+    temp_label.pack()
+
+    temp_label.after(4000, temp_label.destroy)
+
+
+def load():
+    try:
+        with open(os.path.join(file_path, "data.txt"), "r") as data_file:
+            file_name_data.clear()
+            exchanger_data.clear()
+            buddy_data.clear()
+
+            for line in data_file:
+                if line.strip() == "end!@#$%^&*()":
+                    break
+                file_name_data.append(line.strip())
+
+            for line in data_file:
+                if line.strip() == "end!@#$%^&*()":
+                    break
+                exchanger_data.append(line.strip())
+
+            for line in data_file:
+                if line.strip() == "end!@#$%^&*()":
+                    break
+                exchanger_preference.append(line.strip())
+
+            for line in data_file:
+                if line.strip() == "end!@#$%^&*()":
+                    break
+                buddy_data.append(line.strip())
+
+            for line in data_file:
+                if line.strip() == "end!@#$%^&*()":
+                    break
+                buddy_preference.append(line.strip())
+
+            file_name_frame = tk.Frame(scrollable_frame)
+            file_name_frame.pack(side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+
+            exchanger_data_frame = tk.Frame(scrollable_frame)
+            exchanger_data_frame.pack(
+                side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            exchanger_data_title = tk.Label(
+                exchanger_data_frame, text="Input the Name of Exchanger Excel Columns Below: ")
+            exchanger_data_title.grid(row=0, column=0, sticky=tk.W)
+            exchanger_edit_frame = tk.Frame(scrollable_frame)
+            exchanger_edit_frame.pack(
+                side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            delete_exchanger_col_name = tk.Entry(
+                exchanger_edit_frame, width=20)
+            delete_exchanger_col_name.insert(0, "Name of Column to Delete")
+            delete_exchanger_col_name.grid(row=0, column=1, sticky=tk.W)
+            tk.Button(exchanger_edit_frame, text="Delete Column", command=lambda: delete_column(
+                exchanger_data_frame, delete_exchanger_col_name.get(), delete_exchanger_col_name)).grid(row=0, column=2, sticky=tk.W)
+            tk.Button(exchanger_edit_frame, text="Add Column", command=lambda: add_column(
+                exchanger_data_frame)).grid(row=0, column=3, sticky=tk.W)
+
+            exchanger_preference_frame = tk.Frame(scrollable_frame)
+            exchanger_preference_frame.pack(
+                side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            exchanger_preference_title = tk.Label(
+                exchanger_preference_frame, text="Input the Name of Compulsory Columns for Exchanger Below: ")
+            exchanger_preference_title.grid(row=0, column=0, sticky=tk.W)
+
+            buddy_data_frame = tk.Frame(scrollable_frame)
+            buddy_data_frame.pack(side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            buddy_data_title = tk.Label(
+                buddy_data_frame, text="Input the Name of Buddy Excel Columns Below: ")
+            buddy_data_title.grid(row=0, column=0, sticky=tk.W)
+            buddy_edit_frame = tk.Frame(scrollable_frame)
+            buddy_edit_frame.pack(
+                side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            delete_buddy_col_name = tk.Entry(buddy_edit_frame, width=20)
+            delete_buddy_col_name.insert(0, "Name of Column to Delete")
+            delete_buddy_col_name.grid(row=0, column=1, sticky=tk.W)
+            tk.Button(buddy_edit_frame, text="Delete Column", command=lambda: delete_column(
+                buddy_data_frame, delete_buddy_col_name.get(), delete_buddy_col_name)).grid(row=0, column=2, sticky=tk.W)
+            tk.Button(buddy_edit_frame, text="Add Column", command=lambda: add_column(
+                buddy_data_frame)).grid(row=0, column=3, sticky=tk.W)
+
+            buddy_preference_frame = tk.Frame(scrollable_frame)
+            buddy_preference_frame.pack(
+                side=tk.TOP, anchor=tk.W, padx=10, pady=10)
+            buddy_preference_title = tk.Label(
+                buddy_preference_frame, text="Input the Name of Compulsory Columns for Buddy Below: ")
+            buddy_preference_title.grid(row=0, column=0, sticky=tk.W)
+
+            fill_file_name_data(file_name_data, file_name_frame)
+            add_labels(exchanger_data, exchanger_data_frame)
+            add_preference_label(exchanger_preference,
+                                 exchanger_preference_frame)
+            add_labels(buddy_data, buddy_data_frame)
+            add_preference_label(buddy_preference, buddy_preference_frame)
+
+    except FileNotFoundError:
+        with open(file_path, "w") as data_file:
+            pass
+        load()
+
+
+tk.Label(scrollable_frame, text="Welcome to Buddy Matching!").pack()
+
+
+load()
+for i in range(len(file_name_data)):
+    file_name_data[i] = os.path.join(file_path, file_name_data[i])
+
+matcher = Matcher(file_name_data, exchanger_data,
+                  exchanger_preference, buddy_data, buddy_preference)
+
+tk.Button(scrollable_frame, text="Save", command=save).pack()
+tk.Button(scrollable_frame, text="Match", command=matcher.match).pack()
+
+window.mainloop()
---
+++ Matcher.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import random
+
+# Variable
+max_num_of_exchangers_per_buddy = 3
+# Number from 1 to 10, representing the odds of a buddy has the maximum number of exchangers
+percentage_of_buddies_with_max_exchangers = 2
+
+
+class Matcher:
+
+    def __init__(self, file_names, exchanger_data, exchanger_preference, buddy_data, buddy_preference):
+        self.file_names = file_names
+        self.exchanger_data = exchanger_data
+        self.buddy_data = buddy_data
+        self.exchanger_preference = exchanger_preference
+        self.buddy_preference = buddy_preference
+
+    class Exchanger:
+
+        def __init__(self, info, gender, match_gender, faculty, match_faculty, interest):
+            self.info = info
+            self.gender = gender
+            self.match_gender = match_gender
+            self.faculty = faculty
+            self.match_faculty = match_faculty
+            self.interest = interest
+
+        # Adjust the scores accordingly to optimise the significance of each preference
+        def matching_score(self, buddy):
+            score = 0
+            # This ensures that buddies that already have an exchanger are less likely to be matched again
+            # Ensuring fairness in distribution of exchangers to buddies
+            if buddy.has_exchanger():
+                score -= 3
+
+            # If either has a preference for gender, and it matches, add 1 to the score
+            # Else we add 0.5 to the score, as it is easier to match with someone who does not have a preference
+            if self.match_gender or buddy.match_gender:
+                if self.gender == buddy.gender:
+                    score += 1
+            else:
+                score += 0.5
+
+            # Same logic as above, but has a higher weightage of 5
+            if self.match_faculty or buddy.match_faculty:
+                matched = False
+                for faculty in buddy.faculty:
+                    if faculty in self.faculty:
+                        matched = True
+                        break
+                if matched:
+                    score += 5
+                else:
+                    score -= 5
+            else:
+                score += 0.5
+
+            # For eacb similar interests, add 1 to the score
+            for interest in self.interest:
+                if interest in buddy.interest:
+                    score += 1
+
+            return score
+
+    # This code block is to encapsulate the information of a buddy
+
+    class Buddy:
+
+        def __init__(self, info, gender, match_gender, faculty, match_faculty, interest):
+            self.info = info
+            self.faculty = faculty
+            self.match_faculty = match_faculty
+            self.gender = gender
+            self.match_gender = match_gender
+            self.interest = interest
+            self.odds = random.randint(0, 9)
+            self.exchangers = []
+
+        # Check if the buddy has reached the limit of exchangers
+        def is_full(self):
+            if self.odds < 10 - percentage_of_buddies_with_max_exchangers + 1:
+                if max_num_of_exchangers_per_buddy == 1:
+                    return len(self.exchangers) > 0
+                elif max_num_of_exchangers_per_buddy == 2:
+                    return len(self.exchangers) > 1
+                else:
+                    return len(self.exchangers) > max_num_of_exchangers_per_buddy - 2
+            else:
+                return len(self.exchangers) > max_num_of_exchangers_per_buddy - 1
+
+        def has_exchanger(self):
+            return len(self.exchangers) > 0
+
+        def add_exchanger(self, exchanger):
+            if self.is_full():
+                return False
+            self.exchangers.append(exchanger)
+            return True
+
+    def match(self):
+        matchings = []
+
+        # Ensure that the name of the CSV files are correct
+        exchangers_data = pd.read_csv(self.file_names[0])
+        buddies_data = pd.read_csv(self.file_names[1])
+
+        exchangers = []
+        buddies = []
+
+        # Input the column names as per the CSV file
+        # For questions that allow multiple answers, split the answers by ';', ensure
+        # when creating the exchanger/buddy object, input as (answers[:-1]),
+        # where "answers" is the name of the variable of question that allows multiple answers
+        for _, data in exchangers_data.iterrows():
+            info = []
+            exchanger_faculty, exchanger_match_faculty, exchanger_gender, exchanger_match_gender, exchanger_interest = self.exchanger_preference
+            faculty = data[exchanger_faculty].split(';')
+            match_faculty = data[exchanger_match_faculty] == 'Yes'
+            gender = data[exchanger_gender]
+            match_gender = data[exchanger_match_gender] == 'Yes'
+            interest = data[exchanger_interest].split(';')
+            for col in self.exchanger_data:
+                info.append(data[col])
+
+            exchanger = self.Exchanger(info, gender, match_gender,
+                                       faculty[:-1], match_faculty, interest[:-1])
+            exchangers.append(exchanger)
+
+        print("Number of exchangers: ", len(exchangers))
+        # Same logic as above
+        for _, data in buddies_data.iterrows():
+            info = []
+            buddy_faculty, buddy_match_faculty, buddy_gender, buddy_match_gender, buddy_interest = self.buddy_preference
+            faculty = data[buddy_faculty].split(';')
+            match_faculty = data[buddy_match_faculty] == 'Yes'
+            gender = data[buddy_gender]
+            match_gender = data[buddy_match_gender]
+            interest = data[buddy_interest].split(';')
+            for col in self.buddy_data:
+                info.append(data[col])
+            buddy = self.Buddy(info, gender, match_gender,
+                               faculty[:-1], match_faculty, interest[:-1])
+            buddies.append(buddy)
+
+        # Check if there are enough buddies for the exchangers
+        if len(exchangers) > len(buddies) * max_num_of_exchangers_per_buddy:
+            print("There are not enough buddies for the exchangers\n")
+            print("Number of exchangers: ", len(exchangers))
+            print("Number of buddies: ", len(buddies))
+            print("Number of exchangers per buddy: ",
+                  max_num_of_exchangers_per_buddy)
+            print("Ensure that the number of buddy * number of exchangers per buddy is greater than the number of exchangers")
+            exit()
+
+        # Matching algorithm
+        # The alogrithm greedily assigns exchanger to buddies with the highest matching score
+        # Tldr: First come first serve
+        while exchangers and buddies:
+            for exchanger in exchangers:
+                scores = []
+                for buddy in buddies:
+                    score = exchanger.matching_score(buddy)
+                    scores.append(score)
+
+                best_score = max(scores)
+                index = scores.index(best_score)
+                best_buddy = buddies[index]
+
+                if best_buddy.add_exchanger(exchanger):
+                    exchangers.remove(exchanger)
+                    buddies.remove(best_buddy)
+                    buddies.append(best_buddy)
+                else:
+                    buddies.remove(best_buddy)
+                    matchings.append(best_buddy)
+
+        if buddies:
+            for buddy in buddies:
+                matchings.append(buddy)
+        # Export the matchings to a CSV file
+        # Name of each column in the resulting matchings.csv file
+        # Ensure that each column name is unique
+
+        for i in range(len(self.exchanger_data)):
+            self.exchanger_data[i] += ' '
+
+        # # Put the necessary information from the buddies and exchangers into the matchings_data
+        # # Ensure that the column names are the same as the ones above
+
+        results = []
+
+        # Iterate through matchings to populate the rows list
+        for buddy in matchings:
+            for exchanger in buddy.exchangers:
+                result = {self.buddy_data[i]: buddy.info[i]
+                          for i in range(len(self.buddy_data))}
+                result.update({buddy_faculty: ';'.join(buddy.faculty),
+                               buddy_match_faculty: 'Yes' if buddy.match_faculty else 'No preference',
+                               buddy_gender: buddy.gender,
+                               buddy_match_gender: 'Yes' if buddy.match_gender else 'No preference',
+                               buddy_interest: ';'.join(buddy.interest)})
+                result.update({self.exchanger_data[i]: exchanger.info[i]
+                               for i in range(len(self.exchanger_data))})
+
+                result.update({
+                    exchanger_faculty + ' ': ';'.join(exchanger.faculty),
+                    exchanger_match_faculty + ' ': 'Yes' if exchanger.match_faculty else 'No preference',
+                    exchanger_gender + ' ': exchanger.gender,
+                    exchanger_match_gender + ' ': 'Yes' if exchanger.match_gender else 'No preference',
+                    exchanger_interest + ' ': ';'.join(exchanger.interest),
+                })
+
+                # Append the row to the list
+                results.append(result)
+
+        # Create the DataFrame from the results list
+        matchings_data = pd.DataFrame(results, columns=self.buddy_data + [
+            buddy_faculty, buddy_match_faculty,
+            buddy_gender, buddy_match_gender, buddy_interest
+        ] + self.exchanger_data + [
+            exchanger_gender + ' ',
+            exchanger_match_gender + ' ',
+            exchanger_faculty + ' ',
+            exchanger_match_faculty + ' ',
+            exchanger_interest + ' '
+        ])
+
+        for i in range(len(self.exchanger_data)):
+            self.exchanger_data[i] = self.exchanger_data[i][:-1]
+
+        matchings_data.to_csv(self.file_names[2], index=False)
